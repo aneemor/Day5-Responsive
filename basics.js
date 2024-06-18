79b2d6d60698e9309a5d8a08946326eb5dff4af0
Romeena <aneemor@gmail.com>
2024-06-18 20:38:42 +0800

    Added new folder and files

---
+++ basics.js
@@ -0,0 +1,13 @@
+/*
+    VARIABLES == containers/boxed
+        let - mutable/changeable values
+        const - unmutable/unchangeable values
+        var - same as let, but has few scoping differences;
+*/
+
+let myName = "Romeena";
+const myAge = 36;
+
+// display in console
+console.log(nyName);
+console.log(myAge);
